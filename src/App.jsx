f35bbaa60b259cea276b1793f7edb93cbc954fc7
Vikash2048 <rpsvikash16@gmail.com>
2024-02-21 23:55:19 +0530

    add starter files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <section>Hero</section>
+      <section>Parallex</section>
+      <section>Services</section>
+      <section>Parallex</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section>
+    </div>
+  )
+}
+
+export default App
